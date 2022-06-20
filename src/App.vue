77ede329521bb4a960b20de880200c758609eb00
Minino99 <loizzo.pasqualino@gmail.com>
2022-06-20 15:02:31 +0200

    Esercizio completo, bootstrap non funzionante

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <TitleComponent></TitleComponent>
+    <PageHero></PageHero>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TitleComponent from './components/TitleComponent.vue'
+import PageHero from './components/PageHero.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TitleComponent,
+    PageHero,
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/PageHero.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+ <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Jumbotron</h1>
+    <p class="lead">Ecco il bonus di questo magnifico esercizio</p>
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
