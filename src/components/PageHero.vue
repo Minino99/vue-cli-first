0d999bc5ddf2c5e05e28a2b3aa684c9a17730197
Minino99 <loizzo.pasqualino@gmail.com>
2022-06-20 15:17:53 +0200

    Esercizio + bonus completo

--- src/components/PageHero.vue
+++ src/components/PageHero.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
- <div class="jumbotron jumbotron-fluid">
+ <div class="jumbotron jumbotron-fluid bg-warning p-5">
   <div class="container">
     <h1 class="display-4">Jumbotron</h1>
     <p class="lead">Ecco il bonus di questo magnifico esercizio</p>
--- src/components/TitleComponent.vue
+++ src/components/TitleComponent.vue
@@ -1,5 +1,5 @@
 <template>
-<div><h1>La mia prima app con Vue CLI!</h1></div>
+<div><h1 class="text-center p-3">La mia prima app con Vue CLI!</h1></div>
 </template>
 
 <script>
